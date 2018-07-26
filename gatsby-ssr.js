d3406e494419fc314bd1a7de57e4b7c9db6c0134
Fabian Schultz <fabian.schultz@stinkstudios.com>
2018-07-26 14:58:57 -0400

    Fix build.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,13 +8,13 @@ export const replaceRenderer = ({
   replaceBodyHTMLString,
   setHeadComponents,
 }) => {
+  // React Context in SSR/build
+  const ConnectedBody = () => <AppProvider>{bodyComponent}</AppProvider>;
+  replaceBodyHTMLString(renderToString(<ConnectedBody />));
+
   // Add styled-components in SSR/build
   const sheet = new ServerStyleSheet();
   const bodyHTML = renderToString(sheet.collectStyles(<ConnectedBody />));
   const styleElement = sheet.getStyleElement();
   setHeadComponents(styleElement);
-
-  // React Context in SSR/build
-  const ConnectedBody = () => <AppProvider>{bodyComponent}</AppProvider>;
-  replaceBodyHTMLString(renderToString(<ConnectedBody />));
 };
