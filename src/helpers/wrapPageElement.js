aa05f83f69e786452d657c725e244a510deff8fa
Hannes Rabo <hannes.rabo@gmail.com>
2019-01-25 14:55:10 +0100

    Added prop types to the wrapPageElement, fixes eslint error

--- src/helpers/wrapPageElement.js
+++ src/helpers/wrapPageElement.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Transition from 'components/transition';
+import PropTypes from 'prop-types';
 
 const wrapPageElement = ({ element, props }) => {
   return <Transition {...props}>{element}</Transition>;
+};
+
+wrapPageElement.propTypes = {
+  element: PropTypes.any,
+  props: PropTypes.any,
 };
 
 export default wrapPageElement;
