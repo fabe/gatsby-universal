86a42db703a967c158662ec6aadd2f651dc2e53a
Marcelo Lopes Lotufo <marcelo.lopes.lotufo@gmail.com>
2019-05-06 14:05:20 -0300

    Fix png importing problem
    Every time a *.png file was imported an error was happening:
    ```
    error  in ./src/images/test.png
    
    Module parse failed: Unexpected character '�' (1:0)
    ```
    
    Searching for this error I stumbled into [this](https://github.com/jacobmischka/gatsby-plugin-react-svg#i-get-error-module-parse-failed-in-console) and fixed it by changing the include rule for gatsby-plugin-react-svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-react-svg`,
       options: {
         rule: {
-          include: /images/,
+          include: /images\/.*\.svg$/,
         },
       },
     },
